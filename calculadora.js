0715f17fa58da75781152464369fc048d7b4658a
Alessandro Sinibaldi <alsn647@protonmail.com>
2021-10-06 01:24:37 +0200

    Cambiado codigo para que funcione mediante funciones.

--- calculadora.js
+++ calculadora.js
@@ -5,12 +5,88 @@ let operador1;
 let operador2;
 let operadores;
 let operacionValida = false;
-let operadoresDivididos = [];
-let operadoresFiltrados = [];
 let continuar = true;
 let resultado;
+let validarOperadores = function () {
+    operacionValida = false;
+    let operadoresDivididos = [];
+    let operadoresFiltrados = [];
+    while (operacionValida === false) {
+        operadores = prompt("Ingresa dos operadores");
+        operadoresDivididos = operadores.split(" ");
+        for (let i = 0; i < operadoresDivididos.length; i++) {
+            if (operadoresDivididos[i] != "") {
+                operadoresFiltrados.push(operadoresDivididos[i]);
+            }
+        }
+        if (operadoresFiltrados.length === 2) {
+            Number(operadoresFiltrados[0]);
+            Number(operadoresFiltrados[1]);
+            if (isNaN(operadoresFiltrados[0]) === false && isNaN(operadoresFiltrados[1]) === false) {
+                operacionValida = true;
+            } else {
+                alert("Los datos introducidos no son validos");
+                operadoresDivididos = [];
+                operadoresFiltrados = [];
+            }
+        } else {
+            alert("Los datos introducidos no son validos");
+            operadoresDivididos = [];
+            operadoresFiltrados = [];
+        }
+    }
+    return operadoresFiltrados
+}
+
+let suma = (operador1, operador2) => {
+    resultado = (Number(operador1) + Number(operador2));
+    alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
+}
+
+let resta = (operador1, operador2) => {
+    resultado = (Number(operador1) - Number(operador2));
+    alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`);
+}
+
+let multiplicacion = (operador1, operador2) => {
+    resultado = (Number(operador1) * Number(operador2));
+    alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`);
+}
+
+let division = (operador1, operador2) => {
+    resultado = (Number(operador1) / Number(operador2));
+    alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`);
+}
+
 alert("Bienvenido");
 while (continuar === true) {
+    tipoOperacion = validarOperacion();
+    let operadoresFiltrados = []
+    operadoresFiltrados = validarOperadores();
+    operador1 = operadoresFiltrados[0];
+    operador2 = operadoresFiltrados[1];
+    switch (tipoOperacion) {
+        case "+":
+            suma(operador1, operador2);
+            break;
+        case "-":
+            resta(operador1, operador2);
+            break;
+        case "*":
+            multiplicacion(operador1, operador2);
+            break;
+        case "/":
+            division(operador1, operador2);
+            break;
+        default:
+            break;
+    }
+    operacionValida = false;
+    continuar = confirm("Quieres realizar otra operacion?");
+}
+alert("Adios");
+
+function validarOperacion() {
     while (operacionValida === false) {
         tipoOperacion = prompt("Ingresa el tipo de operacion: los simbolos son +, -, * y /");
         if (tipoOperacion != null) {
@@ -29,54 +105,8 @@ while (continuar === true) {
             alert("Tipo de operacion no valida");
         }
     }
-    operacionValida = false;
-    while (operacionValida === false) {
-        operadores = prompt("Ingresa dos operadores");
-        operadoresDivididos = operadores.split(" ");
-        for (let i = 0; i < operadoresDivididos.length; i++) {
-            if (operadoresDivididos[i] != "") {
-                operadoresFiltrados.push(operadoresDivididos[i]);
-            }
-        }
-        if (operadoresFiltrados.length === 2) {
-            Number(operadoresFiltrados[0]);
-            Number(operadoresFiltrados[1]);
-            if (isNaN(operadoresFiltrados[0]) === false && isNaN(operadoresFiltrados[1]) === false) {
-                operador1 = operadoresFiltrados[0];
-                operador2 = operadoresFiltrados[1];
-                operacionValida = true;
-            } else {
-                alert("Los datos introducidos no son validos");
-                operadoresDivididos = [];
-                operadoresFiltrados = [];
-            }
-        } else {
-            alert("Los datos introducidos no son validos");
-            operadoresDivididos = [];
-            operadoresFiltrados = [];
-        }
-    };
-    switch (tipoOperacion) {
-        case "+":
-            resultado = (Number(operador1) + Number(operador2));
-            alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
-            break;
-        case "-":
-            resultado = (Number(operador1) - Number(operador2));
-            alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
-            break;
-        case "*":
-            resultado = (Number(operador1) * Number(operador2));
-            alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
-            break;
-        case "/":
-            resultado = (Number(operador1) / Number(operador2));
-            alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
-            break;
-        default:
-            break;
-    }
-    operacionValida = false;
-    continuar = confirm("Quieres realizar otra operacion?");
+    return tipoOperacion;
 }
-alert("Adios");
+
+
+
