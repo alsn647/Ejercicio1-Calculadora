9c73e1dffc6d66b593f8c8743725a22810edf6f9
Alessandro Sinibaldi <alsn647@protonmail.com>
2021-10-12 18:02:08 +0200

    Implementado objeto Calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,88 @@
 //Autor: Alessandro Sinibaldi - 2º DAW
 "use strict";
+let Calculadora = new Object();
+Calculadora = {
+    lastResult: null,
+    suma: (operador1, operador2) => {
+        if (operador1 === "R" && (isNaN(operador2) === false)) {
+            resultado = (lastResult + Number(operador2));
+            alert(`${lastResult} ${tipoOperacion} ${operador2} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else if ((isNaN(operador1) === false) && operador2 === "R") {
+            resultado = (Number(operador1) + lastResult);
+            alert(`${operador1} ${tipoOperacion} ${lastResult} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else if (operador1 === "R" && operador2 === "R") {
+            resultado = (lastResult + lastResult);
+            alert(`${lastResult} ${tipoOperacion} ${lastResult} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else {
+            resultado = (Number(operador1) + Number(operador2));
+            alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
+            this.lastResult = resultado;
+        }
+    },
+
+    resta: (operador1, operador2) => {
+        if (operador1 === "R" && (isNaN(operador2) === false)) {
+            resultado = (lastResult - Number(operador2));
+            alert(`${lastResult} ${tipoOperacion} ${operador2} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else if ((isNaN(operador1) === false) && operador2 === "R") {
+            resultado = (Number(operador1) - lastResult);
+            alert(`${operador1} ${tipoOperacion} ${lastResult} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else if (operador1 === "R" && operador2 === "R") {
+            resultado = (lastResult - lastResult);
+            alert(`${lastResult} ${tipoOperacion} ${lastResult} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else {
+            resultado = (Number(operador1) - Number(operador2));
+            alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
+            this.lastResult = resultado;
+        }
+    },
+
+    multiplicacion: (operador1, operador2) => {
+        if (operador1 === "R" && (isNaN(operador2) === false)) {
+            resultado = (lastResult * Number(operador2));
+            alert(`${lastResult} ${tipoOperacion} ${operador2} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else if ((isNaN(operador1) === false) && operador2 === "R") {
+            resultado = (Number(operador1) * lastResult);
+            alert(`${operador1} ${tipoOperacion} ${lastResult} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else if (operador1 === "R" && operador2 === "R") {
+            resultado = (lastResult * lastResult);
+            alert(`${lastResult} ${tipoOperacion} ${lastResult} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else {
+            resultado = (Number(operador1) * Number(operador2));
+            alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
+            this.lastResult = resultado;
+        }
+    },
+
+    dividir: (operador1, operador2) => {
+        if (operador1 === "R" && (isNaN(operador2) === false)) {
+            resultado = (lastResult / Number(operador2));
+            alert(`${lastResult} ${tipoOperacion} ${operador2} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else if ((isNaN(operador1) === false) && operador2 === "R") {
+            resultado = (Number(operador1) / lastResult);
+            alert(`${operador1} ${tipoOperacion} ${lastResult} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else if (operador1 === "R" && operador2 === "R") {
+            resultado = (lastResult / lastResult);
+            alert(`${lastResult} ${tipoOperacion} ${lastResult} es: ${resultado}`)
+            this.lastResult = resultado;
+        } else {
+            resultado = (Number(operador1) / Number(operador2));
+            alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
+            this.lastResult = resultado;
+        }
+    },
+}
 let tipoOperacion;
 let operador1;
 let operador2;
@@ -20,9 +103,13 @@ let validarOperadores = function () {
             }
         }
         if (operadoresFiltrados.length === 2) {
-            Number(operadoresFiltrados[0]);
-            Number(operadoresFiltrados[1]);
+            operadoresFiltrados[0];
+            operadoresFiltrados[1];
             if (isNaN(operadoresFiltrados[0]) === false && isNaN(operadoresFiltrados[1]) === false) {
+                operacionValida = true;
+            } else if (((operadoresFiltrados[0] === "R" && isNaN(operadoresFiltrados[1]) === false)) || ((isNaN(operadoresFiltrados[0]) === false) && operadoresFiltrados[1] === "R")) {
+                operacionValida = true;
+            } else if (operadoresFiltrados[0] === "R" && operadoresFiltrados[1] === "R") {
                 operacionValida = true;
             } else {
                 alert("Los datos introducidos no son validos");
@@ -38,25 +125,6 @@ let validarOperadores = function () {
     return operadoresFiltrados
 }
 
-let suma = (operador1, operador2) => {
-    resultado = (Number(operador1) + Number(operador2));
-    alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`)
-}
-
-let resta = (operador1, operador2) => {
-    resultado = (Number(operador1) - Number(operador2));
-    alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`);
-}
-
-let multiplicacion = (operador1, operador2) => {
-    resultado = (Number(operador1) * Number(operador2));
-    alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`);
-}
-
-let division = (operador1, operador2) => {
-    resultado = (Number(operador1) / Number(operador2));
-    alert(`${operador1} ${tipoOperacion} ${operador2} es: ${resultado}`);
-}
 
 alert("Bienvenido");
 while (continuar === true) {
@@ -67,16 +135,16 @@ while (continuar === true) {
     operador2 = operadoresFiltrados[1];
     switch (tipoOperacion) {
         case "+":
-            suma(operador1, operador2);
+            Calculadora.suma(operador1, operador2);
             break;
         case "-":
-            resta(operador1, operador2);
+            Calculadora.resta(operador1, operador2);
             break;
         case "*":
-            multiplicacion(operador1, operador2);
+            Calculadora.multiplicacion(operador1, operador2);
             break;
         case "/":
-            division(operador1, operador2);
+            Calculadora.division(operador1, operador2);
             break;
         default:
             break;
